04608763d77a9b08c2e8ee29eb064a3e1557b1b0
FelipeRomeroDUOC <felipe.romero@users.noreply.github.com>
2026-05-19 02:14:23 -0400

    fix: corregir variable de URL en EstadoFeignClient dentro de pedido-service

--- pedido-service/src/main/java/cl/apipedidos/pedido/client/EstadoFeignClient.java
+++ pedido-service/src/main/java/cl/apipedidos/pedido/client/EstadoFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "estado-service", url = "${estado.service.url}")
+@FeignClient(name = "estado-service", url = "${services.estado-service.url}")
 public interface EstadoFeignClient {
 
     @PostMapping("/api/estados")
